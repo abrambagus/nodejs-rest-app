7a8b053dcf80abf8fb86eed576166797c4310ca3
abram <abrambagus@gmail.com>
2025-10-22 14:50:42 +0700

    handle uplad images

--- backend/controllers/feed.js
+++ backend/controllers/feed.js
@@ -25,13 +25,22 @@ exports.createPost = (req, res, next) => {
     error.statusCode = 422;
     throw error;
   }
+  if (!req.file) {
+    const error = new Error("No image provided.");
+    error.statusCode = 422;
+    throw error;
+  }
+
+  const imageUrl = req.file.path.replace(/\\/g, "/");
   const title = req.body.title;
   const content = req.body.content;
   const post = new Post({
     title: title,
     content: content,
-    imageUrl: "images/duck",
-    creator: { name: "abram" },
+    imageUrl: imageUrl,
+    creator: {
+      name: "Abram",
+    },
   });
   post
     .save()
