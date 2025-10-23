00b165b40d34d05858d1a8d47e3b37e6c35c0c8b
abram <abrambagus@gmail.com>
2025-10-23 15:22:29 +0700

    handle auth middleware

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -64,7 +64,7 @@ exports.login = (req, res, next) => {
           email: loadedUser.email,
           userId: loadedUser._id.toString(),
         },
-        "secret",
+        "somesupersecretsecret",
         { expiresIn: "1h" }
       );
       res.status(200).json({
